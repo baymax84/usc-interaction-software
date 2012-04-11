5f1b48cc6e42ff0b4dada1075d37142e8c471788
Ross Mead <qbitai@gmail.com>
2012-04-11 11:59:01 +0000

    Implemented YAML parsing for servo and servo controller initialization.

--- sparky/nodes/yaml_parser_node.cpp
+++ sparky/nodes/yaml_parser_node.cpp
@@ -1,4 +1,5 @@
 // preprocessor directives
+#include <assert.h>
 #include <fstream>
 #include <iostream>
 #include <sparky/servo_angle_controller.h>
@@ -7,87 +8,111 @@
 typedef std::pair<double, double> JointAnglePair;
 typedef std::pair<JointAnglePair, JointAnglePair> JointLimits;
 
-void operator >>(const YAML::Node &node, pololu::maestro::ServoLimits &servo_limits);
-void operator >>(const YAML::Node &node, pololu::maestro::ServoAngleLimits &angle_limits);
-void operator >>(const YAML::Node &node, JointLimits &joint_limits);
+void operator >>( const YAML::Node &node,
+                  pololu::maestro::ServoAngleLimits &angle_limits );
+void operator >>( const YAML::Node &node, JointLimits &joint_limits );
 
 //
-int main(int argc, char** argv)
+int main( int argc, char** argv )
 {
-  std::string name;
-  int device;
-  int channel;
-  pololu::maestro::ServoLimits servo_limits;
-  pololu::maestro::ServoAngleLimits angle_limits;
-  std::pair< std::pair<double, double>, std::pair<double, double> > joint_limits;
-  double home;
-  double servo_radius;
-  double joint_radius;
+	std::ifstream fin( "params/params.yaml" );
+	YAML::Parser parser( fin );
+	YAML::Node nodes;
+	parser.GetNextDocument( nodes );
 
-  std::ifstream fin("params/params.yaml");
-  YAML::Parser parser(fin);
-  YAML::Node nodes;
-  parser.GetNextDocument(nodes);
-  for (unsigned int i = 0, n = nodes.size(); i < n; ++i)
-  {
-	  nodes[i]["name"] >> name;
+	pololu::maestro::ServoController servo_controller( nodes );
+	printf( "path: %s\n", servo_controller.getPath().c_str() );
+	printf( "n_devices: %d\n", servo_controller.getNumDevices() );
+	printf( "n_channels: %d\n", servo_controller.getNumChannels( 0 ) );
+	printf( "---\n" );
 
-	  nodes[i]["servo"]["device"] >> device;
-	  nodes[i]["servo"]["channel"] >> channel;
-	  nodes[i]["servo"]["servo_limits"] >> servo_limits;
-	  nodes[i]["servo"]["angle_limits"] >> angle_limits;
+	parser.GetNextDocument( nodes );
+	nodes >> servo_controller;
 
-	  nodes[i]["joint"]["joint_servo_mappings"] >> joint_limits;
-	  nodes[i]["joint"]["home"] >> home;
-	  nodes[i]["joint"]["servo_radius"] >> servo_radius;
-	  nodes[i]["joint"]["joint_radius"] >> joint_radius;
+	for ( int device = 0, n_devices = servo_controller.getNumDevices(); device
+	    < n_devices; ++device )
+		for ( int channel = 0, n_channels =
+		    servo_controller.getNumChannels( device ); channel < n_channels; ++channel )
+			if ( servo_controller.getServoEnabled( device, channel ) )
+			{
+				printf( "  servo:\n" );
+				printf( "    device: %d\n", device );
+				printf( "    channel: %d\n", channel );
+				printf( "    servo_limits: [%d, %d]\n",
+				        servo_controller.getServoMinLimit( device, channel ),
+				        servo_controller.getServoMaxLimit( device, channel ) );
+			}
 
-	  printf("- name: %s\n", name.c_str());
-	  printf("  servo:\n");
-	  printf("    device: %d\n", device);
-	  printf("    channel: %d\n", channel);
-	  printf("    servo_limits: [%d, %d]\n", servo_limits.first, servo_limits.second);
-	  printf("    angle_limits: [[%d, %.1f], [%d, %.1f]]\n",
-			 angle_limits.first.first, angle_limits.first.second,
-			 angle_limits.second.first, angle_limits.second.second);
-	  printf("  joint:\n");
-	  printf("    joint_servo_mappings: [[%.1f, %.1f], [%.1f, %.1f]]\n",
-			 joint_limits.first.first, joint_limits.first.second,
-			 joint_limits.second.first, joint_limits.second.second);
-	  printf("    home: %.1f\n", home);
-	  printf("    servo_radius: %.1f\n", servo_radius);
-	  printf("    joint_radius: %.1f\n", joint_radius);
-  }
-
-  return 0;
+	return 0;
 } // main(int, char**)
+/*
+ //
+ int main( int argc, char** argv )
+ {
+ std::string name;
+ int device;
+ int channel;
+ pololu::maestro::ServoLimits servo_limits;
+ pololu::maestro::ServoAngleLimits angle_limits;
+ std::pair<std::pair<double, double>, std::pair<double, double> > joint_limits;
+ double home;
+ double servo_radius;
+ double joint_radius;
 
-void operator >>(const YAML::Node &node, pololu::maestro::ServoLimits &servo_limits)
-{
-  std::pair<int, int> temp;
-  node[0] >> temp.first;
-  node[1] >> temp.second;
-  servo_limits.first = uint16_t(temp.first);
-  servo_limits.second = uint16_t(temp.second);
-} // >>(const YAML::Node &, pololu::maestro::ServoLimits &)
+ std::ifstream fin( "params/params.yaml" );
+ YAML::Parser parser( fin );
+ YAML::Node nodes;
+ parser.GetNextDocument( nodes );
 
-void operator >>(const YAML::Node &node, pololu::maestro::ServoAngleLimits &angle_limits)
+ for ( unsigned int i = 0, n = nodes.size(); i < n; ++i )
+ {
+ nodes[i]["name"] >> name;
+
+ nodes[i]["servo"]["device"] >> device;
+ nodes[i]["servo"]["channel"] >> channel;
+ nodes[i]["servo"]["limits"] >> servo_limits;
+ //nodes[i]["servo"]["servo_limits"] >> servo_limits;
+ nodes[i]["servo"]["angle_limits"] >> angle_limits;
+
+ nodes[i]["joint"]["joint_servo_mappings"] >> joint_limits;
+ nodes[i]["joint"]["home"] >> home;
+ nodes[i]["joint"]["servo_radius"] >> servo_radius;
+ nodes[i]["joint"]["joint_radius"] >> joint_radius;
+
+ printf( "- name: %s\n", name.c_str() );
+ printf( "  servo:\n" );
+ printf( "    device: %d\n", device );
+ printf( "    channel: %d\n", channel );
+ printf( "    servo_limits: [%d, %d]\n", servo_limits.first,
+ servo_limits.second );
+ printf( "    angle_limits: [[%d, %.1f], [%d, %.1f]]\n",
+ angle_limits.first.first, angle_limits.first.second,
+ angle_limits.second.first, angle_limits.second.second );
+ printf( "  joint:\n" );
+ printf( "    joint_servo_mappings: [[%.1f, %.1f], [%.1f, %.1f]]\n",
+ joint_limits.first.first, joint_limits.first.second,
+ joint_limits.second.first, joint_limits.second.second );
+ printf( "    home: %.1f\n", home );
+ printf( "    servo_radius: %.1f\n", servo_radius );
+ printf( "    joint_radius: %.1f\n", joint_radius );
+ }
+
+ return 0;
+ } // main(int, char**)
+ */
+void operator >>( const YAML::Node &node,
+                  pololu::maestro::ServoAngleLimits &angle_limits )
 {
-  std::pair< std::pair<int, double>, std::pair<int, double> > temp;
-  node[0][0] >> temp.first.first;
-  node[0][1] >> temp.first.second;
-  node[1][0] >> temp.second.first;
-  node[1][1] >> temp.second.second;
-  angle_limits.first.first = uint16_t(temp.first.first);
-  angle_limits.first.second = temp.first.second;
-  angle_limits.second.first = uint16_t(temp.second.first);
-  angle_limits.second.second = temp.second.second;
+	node[0][0] >> angle_limits.first.first;
+	node[0][1] >> angle_limits.first.second;
+	node[1][0] >> angle_limits.second.first;
+	node[1][1] >> angle_limits.second.second;
 } // >>(const YAML::Node &, pololu::maestro::ServoAngleLimits &)
 
-void operator >>(const YAML::Node &node, JointLimits &joint_limits)
+void operator >>( const YAML::Node &node, JointLimits &joint_limits )
 {
-  node[0][0] >> joint_limits.first.first;
-  node[0][1] >> joint_limits.first.second;
-  node[1][0] >> joint_limits.second.first;
-  node[1][1] >> joint_limits.second.second;
+	node[0][0] >> joint_limits.first.first;
+	node[0][1] >> joint_limits.first.second;
+	node[1][0] >> joint_limits.second.first;
+	node[1][1] >> joint_limits.second.second;
 } // >>(const YAML::Node &, JointLimits &)
--- sparky/src/servo.cpp
+++ sparky/src/servo.cpp
@@ -3,60 +3,125 @@
 using namespace pololu::maestro;
 
 //
-Servo::Servo(const ServoLimits limits, const bool enabled, const uint16_t target,
-                                     const uint16_t speed, const uint16_t accel) :
-  limits_(limits), enabled_(enabled), target_(target), speed_(speed), accel_(accel)
+Servo::Servo( const ServoLimits limits, const bool enabled,
+              const uint16_t target, const uint16_t speed, const uint16_t accel ) :
+	limits_( limits ), enabled_( enabled ), target_( target ), speed_( speed ),
+	    accel_( accel )
 {
-  //target_ = scaleTargetValue(target);
 } // Servo(const ServoLimits, const uint16_t, const uint16_t, const uint16_t)
 
 //
-Servo::Servo(const Servo &servo)
+Servo::Servo( const Servo &servo )
 {
-  limits_ = servo.limits_;
-  enabled_ = servo.enabled_;
-  target_ = servo.target_;
-  speed_ = servo.speed_;
-  accel_ = servo.accel_;
+	*this = servo;
 } // Servo(const Servo &)
 
 //
-bool Servo::isValidTarget(const uint16_t target) const
+bool Servo::isValid() const
 {
-  return (target >= getMinLimit()) && (target <= getMaxLimit());
+	return (!enabled_) || isValidTarget( target_ );
+} // isValid()
+
+//
+bool Servo::isValidTarget( const uint16_t target ) const
+{
+	return ( target >= getMinLimit() ) && ( target <= getMaxLimit() );
 } // isValidTarget(const uint16_t)
 
 //
-uint16_t Servo::clipTargetValue(const uint16_t target) const
+uint16_t Servo::clipTargetValue( const uint16_t target ) const
 {
-  uint16_t min_limit = getMinLimit();
-  uint16_t max_limit = getMaxLimit();
-  if (target < min_limit)
-    return min_limit;
-  else if (target > max_limit)
-    return max_limit;
-  return target;
+	uint16_t min_limit = getMinLimit();
+	uint16_t max_limit = getMaxLimit();
+	if ( target < min_limit ) return min_limit;
+	else if ( target > max_limit ) return max_limit;
+	return target;
 } // clipTargetValue(const uint16_t)
 
 //
-bool Servo::setTarget(uint16_t target, const bool clip_target)
+bool Servo::setTarget( uint16_t target, const bool clip_target )
 {
-  if (clip_target)
-    target = clipTargetValue(target);
-  else if (!isValidTarget(target))
-    return false;
-  target_ = target;
-  return true;
+	if ( clip_target ) target = clipTargetValue( target );
+	else if ( !isValidTarget( target ) ) return false;
+	target_ = target;
+	return true;
 } // setTarget(uint16_t, const bool)
 
 //
 uint16_t Servo::getMinLimit() const
 {
-  return std::min(limits_.first, limits_.second);
+	return std::min( limits_.first, limits_.second );
 } // getMinLimit()
 
 //
 uint16_t Servo::getMaxLimit() const
 {
-  return std::max(limits_.first, limits_.second);
+	return std::max( limits_.first, limits_.second );
 } // getMaxLimit()
+
+//
+Servo& Servo::operator =( const Servo &servo )
+{
+	if ( &servo != this )
+	{
+		limits_ = servo.limits_;
+		enabled_ = servo.enabled_;
+		target_ = servo.target_;
+		speed_ = servo.speed_;
+		accel_ = servo.accel_;
+	}
+} // =(const Servo &)
+
+//
+void operator >>( const YAML::Node &node, Servo &servo )
+{
+	try
+	{
+		node["limits"] >> servo.limits_;
+	}
+	catch ( YAML::Exception)
+	{
+	}
+
+	try
+	{
+		node["enabled"] >> servo.enabled_;
+	}
+	catch ( YAML::Exception )
+	{
+	}
+
+	try
+	{
+		node["target"] >> servo.target_;
+	}
+	catch ( YAML::Exception )
+	{
+	}
+
+	try
+	{
+		node["speed"] >> servo.speed_;
+	}
+	catch ( YAML::Exception )
+	{
+	}
+
+	try
+	{
+		node["accel"] >> servo.accel_;
+	}
+	catch ( YAML::Exception )
+	{
+	}
+
+	//assert(servo.isValid());
+} // >>(const YAML::Node &, Servo &)
+
+//
+void operator >>( const YAML::Node &node, ServoLimits &servo_limits )
+{
+	assert((node.Type() == YAML::NodeType::Sequence) && (node.size() == 2));
+	node[0] >> servo_limits.first;
+	node[1] >> servo_limits.second;
+} // >>(const YAML::Node &, ServoLimits &)
