56c7b9923d63f6cd0542f9415a90f9d38db71136
Edward T. Kaszubski <ekaszubski@gmail.com>
2012-08-25 21:34:51 -0700

    Added source-based target name resolution and branch-based target version/release resolution to make_deb.

--- make_deb.bash
+++ make_deb.bash
@@ -2,22 +2,26 @@
 
 target=$1
 
-#cd $target
+target_name=`cat $target/debian/control | grep Source: | sed -r 's/Source: *//g'`
 target_branch=`git branch -l | grep \* | sed 's:\* ::g'`
-echo $target_branch
+
+target_release=`echo $target_branch | sed -r 's:[a-zA-Z\-]*-?([0-9\.]+)-?.*:\1:1'`
+target_version=`echo $target_branch | sed -r 's:[a-zA-Z\-]*-?[0-9\.]+-?([0-9\.]+):\1:1'`
+
+target_fullname=$target_name"_"$target_release
+echo $target_fullname
 #latest_log=`git log -n 1`
 #target_version=`echo "$latest_log" | tr '[A-Z]' '[a-z]' | grep date | sed -r 's/date: +//g' | sed -r 's: -.+::g' | sed -r 's: :-:g' | sed 's/:/-/g'`
 #echo $target_version
 #cd ..
 
-deb_target=$target_branch
 mkdir $target-debuild
-cp $target $target-debuild/$deb_target -r
+cp $target $target-debuild/$target_fullname -r
 
-cd $target-debuild/$deb_target && make clean && cd ..
-tar -czf $deb_target.orig.tar.gz $deb_target
+cd $target-debuild/$target_fullname && make clean && cd ..
+tar -czf $target_fullname.orig.tar.gz $target $target_fullname
 
-cd $deb_target
+cd $target_fullname
 
 debuild -S
 sudo pbuilder build ../*.dsc
