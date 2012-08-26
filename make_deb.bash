fe0301a7924d60b65480c22d42e217a42be4821e
Edward T. Kaszubski <ekaszubski@gmail.com>
2012-08-25 22:15:10 -0700

    Added full version/release info to build dir path.

--- make_deb.bash
+++ make_deb.bash
@@ -8,20 +8,22 @@ target_branch=`git branch -l | grep \* | sed 's:\* ::g'`
 target_release=`echo $target_branch | sed -r 's:[a-zA-Z\-]*-?([0-9\.]+)-?.*:\1:1'`
 target_version=`echo $target_branch | sed -r 's:[a-zA-Z\-]*-?[0-9\.]+-?([0-9\.]+):\1:1'`
 
-target_fullname=$target_name"_"$target_release
+target_releasename=$target_name"_"$target_release
+target_fullname=$target_name"_"$target_release"-"$target_version
 echo $target_fullname
+
 #latest_log=`git log -n 1`
 #target_version=`echo "$latest_log" | tr '[A-Z]' '[a-z]' | grep date | sed -r 's/date: +//g' | sed -r 's: -.+::g' | sed -r 's: :-:g' | sed 's/:/-/g'`
 #echo $target_version
 #cd ..
 
-mkdir $target_name-debuild
-cp $target $target_name-debuild/$target_fullname -r
+mkdir $target_fullname-debuild
+cp $target $target_fullname-debuild/$target_releasename -r
 
-cd $target_name-debuild/$target_fullname && make clean && cd ..
-tar -czf $target_fullname.orig.tar.gz $target_fullname
+cd $target_fullname-debuild/$target_releasename && make clean && cd ..
+tar -czf $target_releasename.orig.tar.gz $target_releasename
 
-cd $target_fullname
+cd $target_releasename
 
 debuild -S
 sudo pbuilder build ../*.dsc
