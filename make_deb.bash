9c560bde8529075ab1706c1374c99242f9e95253
Edward T. Kaszubski <ekaszubski@gmail.com>
2012-08-26 16:54:32 -0700

    Added multi-dist/arch/mod support to make_deb and switched from pbuilder to cowbuilder.

--- make_deb.bash
+++ make_deb.bash
@@ -1,6 +1,70 @@
 #!/bin/bash
 
-target=$1
+usage()
+{
+        echo ""
+        echo "Usage: make_deb [-d distname -d distname2] [-a archname -a archname2] [-m modname -m modname2]"
+        echo ""
+}
+
+addDist()
+{
+    if [ "$dists" == "" ]; then
+        dists=$1
+    else
+        dists="$dists $1"
+    fi
+}
+
+addArch()
+{
+    if [ "$archs" == "" ]; then
+        archs=$1
+    else
+        archs="$archs $1"
+    fi
+}
+
+addMod()
+{
+    if [ "$mods" == "" ]; then
+        mods=$1
+    else
+        mods="$mods $1"
+    fi
+}
+
+target=$1; shift
+
+if [ "$target" == "" ]; then usage; exit; fi
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -d )        shift
+                    addDist "$1"
+                    shift
+                    ;;
+        -a )        shift
+                    addArch "$1"
+                    shift
+                    ;;
+        -m )        shift
+                    addMod "$1"
+                    shift
+                    ;;
+        --help )    usage
+                    exit
+                    ;;
+    esac
+done
+
+if [ "$archs" == "" ]; then archs=`dpkg --print-architecture`; fi
+if [ "$dists" == "" ]; then dists=`cat /etc/*-release | grep CODENAME | sed 's:DISTRIB_CODENAME=::g'`; fi
+if [ "$mods" == "" ]; then mods="vanilla"; fi
+
+echo "Selected archs: $archs"
+echo "Selected dists: $dists"
+echo "Selected mods: $mods"
 
 target_name=`cat $target/debian/control | grep Source: | sed -r 's/Source: *//g'`
 target_branch=`git branch -l | grep \* | sed 's:\* ::g'`
@@ -26,4 +90,17 @@ tar -czf $target_releasename.orig.tar.gz $target_releasename
 cd $target_releasename
 
 debuild -S
-sudo pbuilder build ../*.dsc
+
+for dist in $dists; do
+  echo "Starting build for dist group $dist"
+  echo "----------"
+  for arch in $archs; do
+    echo "Starting build for arch group $arch"
+    echo "----------"
+    for mod in $mods; do
+      echo "Starting build for mod group $mod"
+      echo "----------"
+      sudo DIST=$dist ARCH=$arch MOD=$mod cowbuilder --build ../*.dsc
+    done
+  done
+done
