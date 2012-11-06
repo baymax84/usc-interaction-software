3ea84560a32bd8ff2bc2ea1a9f2b2c6f58107dd1
Edward T. Kaszubski <ekaszubski@gmail.com>
2012-11-06 00:42:03 -0800

    Updated local-ppa management script to be distribution-aware.

--- update-local-ppa.bash
+++ update-local-ppa.bash
@@ -10,55 +10,63 @@ apt_update="true"
 
 while [ "$1" != "" ]; do
     case $1 in
-        --no-update )   shift
-                        apt_update="false"
-                        ;;
         * )             shift
                         ;;
     esac
 done
 
-if [ "$apt_update" == "true" ]; then
-    sudo apt-get update
-fi
-
-for item in /var/cache/pbuilder/*/result/*.deb; do
-    basename=`echo "$item" | sed -r 's:(.+)_.+\.deb$:\1:1'`
-    dpkg_info=`dpkg -I $item`
-    version=`echo "$dpkg_info" | grep --max-count 1 "Version: " | sed -r 's/.*Version: (.+)/\1/1'`
+for item in /var/cache/pbuilder/*/result/*.changes; do
+    basename="$item"
+    dpkg_info=`cat $basename`
+    version=`echo "$dpkg_info" | grep --max-count 1 "Version: " | sed -r 's/Version: (.+)/\1/1'`
     release=`echo "$version" | sed -r 's/[0-9\.]*-?([0-9]+)/\1/1'`
-    name=`echo "$dpkg_info" | grep --max-count 1 "Package: " | sed -r 's/.*Package: (.+)/\1/1'`
-    arch=`echo "$dpkg_info" | grep --max-count 1 "Architecture: " | sed -r 's/.*Architecture: (.+)/\1/1'`
-    id=$name$arch
+    api=`echo "$version" | sed -r 's/([0-9\.]*)-?([0-9]+)/\1/1'`
+    dist=`echo "$dpkg_info" | grep --max-count 1 "Distribution: " | sed -r 's/Distribution: (.+)/\1/1'`
+    name=`echo "$dpkg_info" | grep --max-count 1 "Source: " | sed -r 's/Source: (.+)/\1/1'`
+    packages=`echo "$dpkg_info" | grep --max-count 1 "Binary: " | sed -r 's/Binary: (.+)/\1/1'`
+    arch=`echo "$dpkg_info" | grep --max-count 1 "Architecture: " | sed -r 's/Architecture: (.+)/\1/1' | sed -r 's/source //1'`
+#    if [ "$arch" == "all" ]; then
+#        arch_searchterm=*
+#    else
+#        arch_searchterm="$arch"
+#    fi
 
-    if [ "${current_version[$id]}" == "" ]; then
-        current_version[$id]=`apt-cache search $name --names-only --full | grep --max-count 1 "Version: " | sed -r 's/.*Version: (.+)/\1/1'`
+    for package in $packages; do
+        id="$package$dist$arch"
+
+        echo "$item | $version | $release | $api | $dist | $name [ $package ] | $arch | $id"
+
         if [ "${current_version[$id]}" == "" ]; then
-            current_version[$id]="none"
-        else
-            current_release[$id]=`echo "${current_version[$id]}" | sed -r 's/[0-9\.]*-?([0-9]+)/\1/1'`
+            current_deb_path=`ls -r "/home/$USER/archive/"$dist"/"$package"_"$api-*_$arch".deb" | head -n 1`
+#            current_version[$id]=`cat $current_changes_path | grep --max-count 1 "Version: " | sed -r 's/Version: (.+)/\1/1'`
+            current_version[$id]=`[ "$current_deb_path" != "" ] && dpkg --info $current_deb_path | grep --max-count 1 "Version: " | sed -r 's/.*Version: (.+)/\1/1'`
+            if [ "${current_version[$id]}" == "" ]; then
+                current_version[$id]="none"
+            else
+                current_release[$id]=`echo "${current_version[$id]}" | sed -r 's/[0-9\.]*-?([0-9]+)/\1/1'`
+            fi
+            echo -e "\e[34mNewest version of $package $dist $arch set to \e[4m${current_version[$id]}\e[0m"
         fi
-        echo -e "\e[34mNewest version of $name $arch set to \e[4m${current_version[$id]}\e[0m"
-    fi
 
 #    echo "$item"
 #    echo "$name : [ $version ] ? [ $current_version ] | [ $release ] ? [ $current_release ]"
 
-    if [ "${current_release[$id]}" != "" ]; then
-        if [ $release -lt ${current_release[$id]} ]; then
-            echo -e "\e[33mPackage [ $name \e[4m$version\e[0;33m $arch ] is out-of-date; newest version is [ ${current_version[$id]} ]\e[0m"
-#            dcut -i $basename*.changes local
-            sudo rm $basename*
-        elif [ $release -gt ${current_release[$id]} ]; then
-            echo -e "\e[0;36mPackage [ $name \e[4m$version\e[0;36m $arch ] is newer than the apt cache; invoking dput\e[0m"
-            dput -U local $basename*.changes
+        if [ "${current_release[$id]}" != "" ]; then
+            if [ $release -lt ${current_release[$id]} ]; then
+                echo -e "\e[33mPackage [ $package \e[4m$version\e[0;33m $dist $arch ] is out-of-date; newest version is [ ${current_version[$id]} ]\e[0m"
+#                dcut -i $basename*.changes local
+                sudo rm $basename*
+            elif [ $release -gt ${current_release[$id]} ]; then
+                echo -e "\e[0;36mPackage [ $package \e[4m$version\e[0;36m $dist $arch ] is newer than the apt cache; invoking dput\e[0m"
+                dput -U local $basename
+            else
+                echo -e "\e[0;32mPackage [ $package \e[4m$version\e[0;32m $dist $arch ] is up-to-date\e[0m"
+            fi
         else
-            echo -e "\e[0;32mPackage [ $name \e[4m$version\e[0;32m $arch ] is up-to-date\e[0m"
+            echo -e "\e[0;35mPackage [ $package \e[4m$version\e[0;35m $dist $arch ] was not found in the apt cache; invoking dput\e[0m"
+            dput -U local $basename
         fi
-    else
-        echo -e "\e[0;35mPackage [ $name \e[4m$version\e[0;35m $arch ] was not found in the apt cache; invoking dput\e[0m"
-        dput -U local $basename*.changes
-    fi
+    done
 done
 
 echo "----------"
