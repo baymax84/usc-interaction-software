e93dbaec70542d95d186c398fbce67d893fcb785
Edward T. Kaszubski <ekaszubski@gmail.com>
2012-11-06 02:52:19 -0800

    Added a dist-upgrade hook and an update-all script to pull in all new packages.

---
+++ update-all.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+chmod +x ~/.pbuilder/hooks/F00dist-upgrade
+for dist in maverick oneiric precise; do
+  for arch in i386 amd64; do
+    for mod in vanilla interaction ros-diamondback ros-electric ros-fuerte ros-groovy; do
+      sudo DIST=$dist ARCH=$arch MOD=$mod cowbuilder --login --save-after-login
+    done
+  done
+done
+chmod -x ~/.pbuilder/hooks/F00dist-upgrade
