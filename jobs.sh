ae6f93b373bdedfa6d83be7821a23606b0f74c66
Edward T. Kaszubski <ekaszubski@gmail.com>
2013-03-11 11:23:25 -0700

    Modified cron-based build to log to separate files for easier readability. Added example crontab and env files. Added zipped archive of logs to mail command and end of cron run.

--- jobs.sh
+++ jobs.sh
@@ -3,15 +3,20 @@
 echo "`date` cron start"
 echo "----------"
 
-bash ~/cron/update-sources.bash
+bash ~/cron/update-sources.bash > $CRON_LOG_DIR/update-sources.log 2>&1
 
-bash ~/build_system/update-all.bash
+bash ~/build_system/update-all.bash > $CRON_LOG_DIR/update-all.log 2>&1
 
-bash ~/cron/build-debs.bash
+bash ~/cron/build-debs.bash > $CRON_LOG_DIR/build-debs.log 2>&1
 
 echo "----------"
 echo "`date` cron stop"
 
-mail -s "calculon cron log `date +%F-%R-%Z`" ekaszubski@gmail.com < /home/ekaszubski/cron/log
+# when running, the log dir is known by its start time; when completed, it's known by its end time
 
-mv /home/ekaszubski/cron/log "/home/ekaszubski/cron/logs/log-`date +%F-%R`"
+STOP_TIME=`date +%F-%R-%S-%N`
+NEW_CRON_LOG_DIR=$HOME/cron/logs/$STOP_TIME
+
+mv $CRON_LOG_DIR $NEW_CRON_LOG_DIR
+zip -r $HOME/cron/logs/$STOP_TIME.zip $NEW_CRON_LOG_DIR -x "*cron.log"
+cat $NEW_CRON_LOG_DIR/cron.log | mutt -s "calculon cron log $STOP_TIME" -a $HOME/cron/logs/$STOP_TIME.zip -- ekaszubski@gmail.com 
